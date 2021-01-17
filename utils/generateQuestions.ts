434cdd308fd6f58d6d1f4139317dbc2fdb757497
Adrien Lacroix <dev@lx-consulting.fr>
2021-01-17 23:44:54 +0100

    :bug: add missing regions in questions

--- utils/generateQuestions.ts
+++ utils/generateQuestions.ts
@@ -1,7 +1,10 @@
 import alsace from "@assets/data/alsace.json";
 import bordeaux from "@assets/data/bordeaux.json";
 import bourgogne from "@assets/data/bourgogne.json";
+import champagne from "@assets/data/champagne.json";
+import corse from "@assets/data/corse.json";
 import jura from "@assets/data/jura.json";
+import languedoc from "@assets/data/languedoc.json";
 import loire from "@assets/data/loire.json";
 import provence from "@assets/data/provence.json";
 import rhone from "@assets/data/rhone.json";
@@ -10,7 +13,10 @@ const availableData = [
   ...alsace.map(({ ...rest }) => ({ ...rest, region: "alsace" })),
   ...bordeaux.map(({ ...rest }) => ({ ...rest, region: "bordeaux" })),
   ...bourgogne.map(({ ...rest }) => ({ ...rest, region: "bourgogne" })),
+  ...champagne.map(({ ...rest }) => ({ ...rest, region: "champagne" })),
+  ...corse.map(({ ...rest }) => ({ ...rest, region: "corse" })),
   ...jura.map(({ ...rest }) => ({ ...rest, region: "jura" })),
+  ...languedoc.map(({ ...rest }) => ({ ...rest, region: "languedoc" })),
   ...loire.map(({ ...rest }) => ({ ...rest, region: "loire" })),
   ...provence.map(({ ...rest }) => ({ ...rest, region: "provence" })),
   ...rhone.map(({ ...rest }) => ({ ...rest, region: "rhone" })),
